fd481f51269ec9ff087d8e97ec2c0505ce472c85
Ciaran Schembri <schembri@toby.dartmouth.edu>
2023-05-19 17:06:18 -0400

    more tables

--- misc/overnight-magma.m
+++ misc/overnight-magma.m
@@ -1,7 +1,7 @@
 for D in [6] do
-  for N in [4,6] do 
+  for N in [3,4,6] do 
     for del in [1,2] do 
-	  rhocirc:=AllEnhancedSubgroups(D,del,N : PQMtorsion:=true, verbose:=true, minimal:=false, lowgenus:=true);
+	  rhocirc:=AllEnhancedSubgroups(D,del,N : PQMtorsion:=false, verbose:=true, minimal:=false, lowgenus:=false, write:=true);
 	  print "";
 	end for;
   end for;
