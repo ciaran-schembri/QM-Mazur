89d7e744ee0e9ab8a709556cf7afa0827eaf86cf
Ciaran Schembri <schembri@toby.dartmouth.edu>
2023-05-25 10:47:56 -0400

    N6-deg6

---
+++ misc/log-Ari-Z4.m
@@ -0,0 +1,16 @@
+Magma V2.27-7     Mon May 22 2023 22:07:27 on toby     [Seed = 3857547430]
+
++-------------------------------------------------------------------+
+|       This copy of Magma has been made available through a        |
+|                   generous initiative of the                      |
+|                                                                   |
+|                         Simons Foundation                         |
+|                                                                   |
+| covering U.S. Colleges, Universities, Nonprofit Research entities,|
+|               and their students, faculty, and staff              |
++-------------------------------------------------------------------+
+
+Type ? for help.  Type <Ctrl>-D to quit.
+
+Loading startup file "/home/schembri/.magmarc"
+
--- misc/overnight-magma.m
+++ misc/overnight-magma.m
@@ -1,6 +1,6 @@
 for D in [6] do
-  for N in [3,4,6] do 
-    for del in [1,2] do 
+  for N in [6] do 
+    for del in [6] do 
 	  rhocirc:=AllEnhancedSubgroups(D,del,N : PQMtorsion:=false, verbose:=true, minimal:=false, lowgenus:=false, write:=true);
 	  print "";
 	end for;
