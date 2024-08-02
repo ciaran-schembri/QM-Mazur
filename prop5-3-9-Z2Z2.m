3c994ac645ba1f91d587e55a3f7fd214866e4547
Ciaran Schembri <ciaran.schembri@dartmouth.edu>
2024-08-02 16:18:58 -0400

    renamed

--- prop5-3-9-Z2Z2.m
+++ prop5-3-9-Z2Z2.m
@@ -1,12 +1,11 @@
 //////////////////////////////////////////////
 //this loads the hyperelliptic polynomial which defines the genus 2 curves with Igusa invariants below.
-load "QM-Mazur/misc/prop4-3-9-Z2Z2-defining-equations.m";
+load "prop5-3-9-Z2Z2-defining-equations.m";
 
 X := Curve(ProjectiveSpace(PolynomialRing(Rationals(), 2)));
 KX<x> := FunctionField(X);
 
 j:=(-64*x^20 + 256*x^16 - 384*x^12 + 256*x^8 - 64*x^4)/(x^24 + 42*x^20 + 591*x^16 + 2828*x^12 + 591*x^8 + 42*x^4 + 1);
-//j:=phi;
 J2:=12*(j+1);
 J4:=6*(j^2+j+1);
 J6:=4*(j^3-2*j^2+1);
